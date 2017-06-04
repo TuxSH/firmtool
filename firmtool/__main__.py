4166bb68af9bd77747fbf076bcf47bc68a37e933
TuxSH <tuxsh@sfr.fr>
2017-06-05 00:28:03 +0200

    Bump version number

--- firmtool/__main__.py
+++ firmtool/__main__.py
@@ -3,7 +3,7 @@
 __author__    = "TuxSH"
 __copyright__ = "Copyright (c) 2017 TuxSH"
 __license__   = "BSD"
-__version__   = "1.0"
+__version__   = "1.1"
 
 """
 Parses, extracts, and builds 3DS firmware files
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='firmtool',
-    version='1.0',
+    version='1.1',
     description='Parses, extracts, and builds 3DS firmware files',
     license='BSD',
     keywords='3DS firmware parse extract build',
